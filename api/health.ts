aef549f71fb65b359ae93af3fa8a3660810e480b
Saumil Patel <saumilpatel1@gmail.com>
2026-02-23 22:11:49 +0200

    Package fix for successful deployment

--- api/health.ts
+++ api/health.ts
@@ -2,5 +2,5 @@ export default function handler(_req: any, res: any) {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res
     .status(200)
-    .send(JSON.stringify({ ok: true, service: "github-stats-vercel" }));
+    .send(JSON.stringify({ ok: true, service: "gh-stats", version: "1.0.0" }));
 }
